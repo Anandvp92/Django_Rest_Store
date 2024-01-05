f6b60aa7ab4c84fd39f65422fdebdf0ebb71952f
Anandvp <anandvp92@gmail.com>
2024-01-05 18:46:25 +0530

    second commit

--- Backend/frontend/src/App.jsx
+++ Backend/frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from  './Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
       <div>
+      <Login></Login>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ Backend/frontend/src/Login.jsx
@@ -0,0 +1,8 @@
+function Login(){
+
+    return(
+    <h1>Hello world</h1>
+    );
+}
+
+export default Login;
